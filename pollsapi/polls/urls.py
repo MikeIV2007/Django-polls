7e9624c05143e6963ccf0ff535c4a5bc2223a640
MikeIV2007 <130536460+MikeIV2007@users.noreply.github.com>
2024-08-02 14:35:21 +0300

    urls_structure

--- pollsapi/polls/urls.py
+++ pollsapi/polls/urls.py
@@ -7,9 +7,16 @@ from .apiviews import PollList, PollDetail, ChoiceList, CreateVote
 #     path("polls/<int:pk>/", polls_detail, name="polls_detail"),
 # ]
 
+# urlpatterns = [
+#     path("polls/", PollList.as_view(), name = "pols_list"),
+#     path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail"),
+#     path("choices/", ChoiceList.as_view(), name='choice_list'),
+#     path("vot/", CreateVote.as_view(), name = "create_vote")
+# ]
+
 urlpatterns = [
     path("polls/", PollList.as_view(), name = "pols_list"),
     path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail"),
-    path("choices/", ChoiceList.as_view(), name='choice_list'),
-    path("vot/", CreateVote.as_view(), name = "create_vote")
+    path("polls/<int:pk>/choices/", ChoiceList.as_view(), name = "choice_list"),
+    path("polls/<int:pk>/choices/<int:choice_pk>/vote/", CreateVote.as_view(), name = "create_vote")
 ]
