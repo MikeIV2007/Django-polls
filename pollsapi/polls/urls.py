6ed825f5523dd205a3a8c20974c2b836829e50f3
MikeIV2007 <130536460+MikeIV2007@users.noreply.github.com>
2024-08-02 10:49:42 +0300

    serializer

--- pollsapi/polls/urls.py
+++ pollsapi/polls/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from .views import polls_list, polls_detail
+from .apiviews import PollList, PollDetail
+
+# urlpatterns = [
+#     path("polls/", polls_list, name="polls_list"),
+#     path("polls/<int:pk>/", polls_detail, name="polls_detail"),
+# ]
 
 urlpatterns = [
-    path("polls/", polls_list, name="polls_list"),
-    path("polls/<int:pk>/", polls_detail, name="polls_detail"),
+    path("polls/", PollList.as_view(), name = "pols_list"),
+    path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail")
 ]
